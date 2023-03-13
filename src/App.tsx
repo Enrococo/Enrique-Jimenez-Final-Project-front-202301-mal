597ac5e265b9024c8cfe957a76d18afbf2940374
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-14 00:08:01 +0100

    Modify app to host WIP page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img
+        src="/assets/estora-modular.gif"
+        alt="Gif estora modular"
+        className="App-logo"
+      />
+      <img src="/assets/estora-logo.svg" alt="logo estora" />
+      <h2 className="Wip">
+        Estamos trabajando duro para que pronto puedas crear tu propia alfombra
+        Modular
+      </h2>
+    </div>
+  );
 }
 
 export default App;
