17e5119d878a6aba96ebc214d7f16b026cef9be2
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-16 16:38:41 +0100

    Add middleware to store and and update link styles in index

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,6 +5,18 @@ export const store = configureStore({
   reducer: {
     login: loginReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ['form/postNewLogin/fulfilled'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['meta.arg'],
+        // Ignore these paths in the state
+        ignoredPaths: ['login'],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
