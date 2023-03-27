e39360df35fbfb4a1100b6e1adad9c35573f86b2
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-27 20:26:56 +0200

    Add form component, slice and test

---
+++ src/features/customize-form/CustomizeForm.test.tsx
@@ -0,0 +1,127 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import html2canvas from 'html2canvas';
+import { Provider } from 'react-redux';
+import { store } from '../../app/store';
+import { server } from '../../mocks/server';
+import userEvent from '@testing-library/user-event';
+import { CustomizeForm } from './CustomizeForm';
+import { MemoryRouter } from 'react-router-dom';
+
+jest.mock('html2canvas');
+const html2CanvasMock = jest.mocked(html2canvas);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  jest.resetAllMocks();
+  jest.clearAllMocks();
+});
+
+afterAll(() => server.close());
+
+describe('Given a Customize form component', () => {
+  test('When rendering the form, it should be on the document', () => {
+    render(
+      <Provider store={store}>
+        <CustomizeForm />
+      </Provider>
+    );
+    const button = screen.getByRole('button');
+    const formElement = screen.getByRole('form');
+    expect(formElement).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+  test('when the user tries to create a customized carpet, and this is created, it should show a possitive feedback', async () => {
+    window.HTMLCanvasElement.prototype.toDataURL = jest.fn(
+      () => 'data:image/png;base64,aGVsbG8gd29ybGQ='
+    );
+    globalThis.fetch = jest.fn().mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValue({
+        msg: 'Tu alfombra se ha tejido correctamente',
+      }),
+    });
+    html2CanvasMock.mockImplementation(async () =>
+      document.createElement('canvas')
+    );
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <CustomizeForm />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const createButton = screen.getByRole('button');
+
+    await userEvent.click(createButton);
+
+    await waitFor(async () => {
+      const successMessage = await screen.findByLabelText('success_text');
+      expect(successMessage).toHaveTextContent(
+        'Tu diseño se ha creado correctamente'
+      );
+    });
+  });
+
+  test('when the user tries to create a customized carpet, and there is a mistake, it should show a negative feedback', async () => {
+    window.fetch.prototype.blob = jest.fn(() => new Blob());
+    window.HTMLCanvasElement.prototype.toDataURL = jest.fn(
+      () => 'data:image/png;base64,aGVsbG8gd29ybGQ='
+    );
+    globalThis.fetch = jest.fn().mockResolvedValueOnce({
+      ok: false,
+      json: jest.fn().mockResolvedValue({
+        msg: 'Ha habido un error',
+      }),
+    });
+    html2CanvasMock.mockImplementation(async () =>
+      document.createElement('canvas')
+    );
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <CustomizeForm />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const createButton = screen.getByRole('button');
+
+    await userEvent.click(createButton);
+
+    await waitFor(async () => {
+      const errorMessage = await screen.findByLabelText('error');
+      expect(errorMessage).toHaveTextContent('Ha habido un error');
+    });
+  });
+
+  // test('When the user logs in with an unregistered account, it should show an error message', async () => {
+  //   server.use(...errorHandlers);
+
+  //   render(
+  //     <Provider store={store}>
+  //       <MemoryRouter>
+  //         <Login />
+  //       </MemoryRouter>
+  //     </Provider>
+  //   );
+
+  //   const submitButton = screen.getByRole('button');
+
+  //   fireEvent.click(submitButton);
+
+  //   const loading = screen.getByLabelText('loading');
+  //   expect(loading).toBeInTheDocument();
+
+  //   await waitFor(async () => {
+  //     const errorMessage = await screen.findByLabelText('error');
+  //     expect(errorMessage).toHaveTextContent(
+  //       'There is no registered user with this email and password'
+  //     );
+  //   });
+  // });
+});
---
+++ src/features/customize-form/CustomizeForm.tsx
@@ -0,0 +1,102 @@
+import html2canvas from 'html2canvas';
+import { Link } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { selectModularItemsUrl } from '../customize/modular-item/modularItemSlice';
+import { postNewModular, selectCustomize } from './CustomizeFormSlice';
+import { CustomizeFormStyled } from './CustomizeFormStyled';
+
+export const CustomizeForm = () => {
+  const dispatch = useAppDispatch();
+  const selectState = useAppSelector(selectModularItemsUrl);
+  const { formMsg, formState } = useAppSelector(selectCustomize);
+
+  const showUserFeedback = () => {
+    switch (formState) {
+      case 'error':
+        return (
+          <span className="error info" aria-label="error">
+            {formMsg}
+          </span>
+        );
+      case 'success':
+        return (
+          <span className="success_text info" aria-label="success_text">
+            Tu diseño se ha creado correctamente
+          </span>
+        );
+
+      case 'idle':
+        return (
+          <span className="loading info" aria-label="loading">
+            {' '}
+            Cargando...
+          </span>
+        );
+    }
+  };
+  let screenshot;
+  let canvas: HTMLCanvasElement;
+  function dataURItoBlob(dataURI: string) {
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+    else byteString = unescape(dataURI.split(',')[1]);
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ia], { type: mimeString });
+  }
+
+  return (
+    <>
+      <CustomizeFormStyled
+        aria-label="form"
+        className="login__form"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+
+          canvas = await html2canvas(
+            document.querySelector('#board') as HTMLElement
+          );
+
+          screenshot = canvas.toDataURL();
+          const screenshotFile = await dataURItoBlob(screenshot);
+          formData.append('thumb', screenshotFile);
+          dispatch(postNewModular(formData));
+        }}
+      >
+        <div className="customize-form__name__container" aria-label="customize">
+          <h3 className="customize-form__title">
+            3. Da un nombre a tu creación:
+          </h3>
+          <input className="customize-form__name" type="text" name="name" />
+        </div>
+        <div className="customize-form__price__button-containter">
+          <h3 className="customize-form__price">Precio:</h3>
+          <input
+            type="text"
+            name="price"
+            className="customize-form__price"
+            value={`${selectState.length * 30} €`}
+            readOnly
+          />
+
+          <div className="create__button-container">
+            <button
+              type="submit"
+              value="Create"
+              className="create__button"
+              disabled={formState === 'idle' ? true : false}
+            >
+              Crear
+            </button>
+          </div>
+        </div>
+        <div>{showUserFeedback()}</div>
+      </CustomizeFormStyled>
+    </>
+  );
+};
