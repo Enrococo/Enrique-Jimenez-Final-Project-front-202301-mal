1b8de8ae8c23d2077fba22a003dbd0c76423869f
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-27 20:24:48 +0200

    Change customize page to add form and update variable styles

--- src/router/pages/customize/CustomizeStyled.ts
+++ src/router/pages/customize/CustomizeStyled.ts
@@ -4,6 +4,12 @@ export const CustomizeStyled = styled.div`
   height: 100%;
   display: grid;
   grid-template-columns: 50% 50%;
+  grid-template-rows: 100%;
+  @media screen and (max-width: 1040px) {
+    display: flex;
+    flex-direction: column;
+    overflow: scroll;
+  }
 
   .columnOne {
     justify-content: center;
@@ -18,5 +24,18 @@ export const CustomizeStyled = styled.div`
     justify-content: center;
     flex-direction: column;
     align-items: center;
+  }
+
+  .custom__title {
+    padding: 1rem;
+    width: 100%;
+    text-align: start;
+  }
+
+  .custom__subtitle {
+    padding: 1rem 2rem;
+    width: 90%;
+    min-width: 45%;
+    text-align: start;
   }
 `;
